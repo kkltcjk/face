ded03fc8a57ebb7e30fe781e6b68347266213213
JackChan <jack.chen.8@qq.com>
2018-05-18 20:49:09 +0800

    change cluster path

--- face/cluster/train.py
+++ face/cluster/train.py
@@ -1,7 +1,10 @@
+import os
+
 from face.cluster import Cluster
 from face.cluster.cluster import do_cluster
 
 
 class TrainClusterV1(Cluster):
     def run(self):
-        do_cluster(self.ddir)
+        cluster_path = os.path.join(self.ddir, 'cluster')
+        do_cluster(cluster_path)
